7f06f0ceaf5f1c00a14e734205bcc9368a2248d2
Dan <dan@fairedev.com>
2020-07-27 21:57:20 -0400

    trying out pigpio library to send pulse

--- src/dist.py
+++ src/dist.py
@@ -1,53 +1,92 @@
-#Libraries
 import RPi.GPIO as GPIO
 import time
 import pigpio
 
  
-#set GPIO Pins
+# GPIO Pins on the RPi
 GPIO_TRIGGER = 18
 GPIO_ECHO = 24
- 
-pi = pigpio.pi()
-print("connected=", pi.connected)
 
-pi.set_mode(GPIO_TRIGGER, pigpio.OUTPUT)
-pi.set_mode(GPIO_ECHO, pigpio.INPUT)
-
-pi.callback(GPIO_TRIGGER, pigpio.EITHER_EDGE, callback_func)
-pi.callback(GPIO_ECHO, pigpio.EITHER_EDGE, callback_func)
+SPEED_OF_SOUND = 34300  # cm/s
+LOW = 0
+HIGH = 1
 
 
-def callback_func(gpio, level, tick):
-    pass
-
-def distance():
+class UltrasoundSensor:
     """
-    Send a sound pulse by setting TRIG pin high.  When signal sent, the ECHO pin also goes high until the response
-    is received.  The elapsed round-trip time (RTT) is therefore when the ECHO goes high until it goes low again.
+    This class controls the HC-SR04 ultransonic sensor using pigpio
+    to send a pulse out and read how long it takes to come back
 
+    Based on Sonar Ranger example from pigpio library here:
+    http://abyz.me.uk/rpi/pigpio/examples.html#Python%20code
     """
-    # send out a pulse from TRIG
-    pi.gpio_trigger(GPIO_TRIGGER, pulse_len=10, level=1) 
-    StartTime = time.time()
 
-    pi.set_watchdog(GPIO_ECHO, wdog_timeout=1000)
-    StopTime = time.time()
+    def __init__(self):
+        self.pi = pigpio.pi()
+        print("connected=", self.pi.connected)
 
-    print("logged start {} and stop {}".format(StartTime, StopTime))
+        self.pi.set_mode(GPIO_TRIGGER, pigpio.OUTPUT)
+        self.pi.set_mode(GPIO_ECHO, pigpio.INPUT)
 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
- 
+        self.pi.callback(GPIO_TRIGGER, pigpio.EITHER_EDGE, self.ping_callback_func)
+        self.pi.callback(GPIO_ECHO, pigpio.EITHER_EDGE, self.ping_callback_func)
+
+        self.high_time = None
+        self.time_elapsed = None
+        self.ping_received = False
+        self.triggered = False
+
+    def distance(self):
+        """
+        Send a sound pulse by setting TRIG pin high.  When signal sent, the ECHO pin also goes high until the response
+        is received.  The elapsed round-trip time (RTT) is therefore when the ECHO goes high until it goes low again.
+
+        """
+        self.ping_received = False
+
+        # send out a pulse from TRIG
+        self.pi.gpio_trigger(GPIO_TRIGGER, pulse_len=10, level=1)
+        start_time = time.time()
+
+        while not self.ping_received:
+            # wait for ping to come back to callback
+            if (time.time() - start_time) > 5.0:
+                print("Ping not received... timed out")
+                return -1
+            time.sleep(0.001)
+
+        # dist = time * speed
+        round_trip_distance = self.time_elapsed * SPEED_OF_SOUND
+        return round_trip_distance / 2
+
+    def ping_callback_func(self, gpio, level, tick):
+        """
+        Set self.time elapsed after a signal change
+        :param gpio: pin that changed state
+        :param level: 0=low, 1=high, 2=no change
+        :param tick: microseconds since boot
+        """
+        if gpio == GPIO_TRIGGER:
+            if level == LOW:
+                # trigger just sent
+                self.triggered = True
+                self.high_time = None
+        else:
+            if self.triggered:
+                if level == HIGH:
+                    self.high_time = tick
+                else:
+                    if self.high_time is not None:
+                        self.time_elapsed = tick - self.high_time
+                        self.high_time = None
+                        self.ping_received = True
+
+
 if __name__ == '__main__':
+    ranger = UltrasoundSensor()
     try:
         while True:
-            dist = distance()
+            dist = ranger.distance()
             print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
  
@@ -55,4 +94,4 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
-        pi.stop()
+        ranger.pi.stop()
